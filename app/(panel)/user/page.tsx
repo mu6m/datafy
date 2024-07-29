f16bb8605379bc20f069e98885ad9157cb6ce4c6
mu6m <theledraa@gmail.com>
2024-07-29 03:35:56 +0300

    hi

--- app/(panel)/user/page.tsx
+++ app/(panel)/user/page.tsx
@@ -69,7 +69,10 @@ export default function Comp() {
 					<tbody>
 						{data.items.map((item: any) => {
 							return (
-								<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+								<tr
+									key={item.title}
+									className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+								>
 									<th
 										scope="row"
 										className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
